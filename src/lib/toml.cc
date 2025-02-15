2eb2e0057a289db3f66d90dea9bbdc8acea96988
anders-erik <anderserikerik@gmail.com>
2025-02-15 17:24:08 +0100

    TOML parser: refactored and more docs;

--- src/lib/toml.cc
+++ src/lib/toml.cc
@@ -15,6 +15,53 @@
 namespace plib {
 
 
+    // START TOML
+
+    void TOML::load(std::string file_path) {
+        tables.clear();
+
+        TOML_Table* new_table;
+        TOML_String root_table_name = "ROOT_TABLE";
+        std::vector <std::string>   current_table_lines = {};
+
+
+        std::string                 file_contents = plib::fs_cat(file_path);
+        std::vector <std::string>   file_lines = plib::std_string::split(file_contents, '\n');
+
+
+        for (std::string line : file_lines) {
+
+            bool is_table_label = is_table_label_line(line);
+
+            if (is_table_label) {
+
+                new_table = new TOML_Table(current_table_lines);
+
+                tables.push_back(new_table);
+            }
+
+            current_table_lines.push_back(line);
+        }
+
+
+        // Make sure the last table is parsed
+        if (current_table_lines.size() > 0) {
+
+            TOML_Table* new_table = new TOML_Table(current_table_lines);
+
+            tables.push_back(new_table);
+
+            // All KVs before first table label is referred to as the root table per the specs
+            tables[0]->name = root_table_name;
+        }
+
+
+
+        std::cout << "TOML LOADING DONE" << std::endl;
+
+    }
+
+
     TOML_Table* TOML::find_table(TOML_String table_name) {
 
         for( TOML_Table* table : tables){
@@ -26,150 +73,49 @@ namespace plib {
     }
 
 
-    void TOML_Value::set_value(TOML_String string) {
-        type = TOML_ValueType::STRING;
-        this->string = string;
-    }
-    void TOML_Value::set_value(TOML_Array array) {
-        type = TOML_ValueType::ARRAY;
-        this->array = array;
-    }
+    // END TOML
 
+    // START TOML TABLE
 
-    void TOML::load(std::string file_path) {
+    TOML_Table::TOML_Table(std::vector<std::string> lines) {
 
-        tables.clear();
+        name = "_";
 
+        for (std::string line : lines) {
 
-        std::string                 file_contents = plib::fs_cat(file_path);
-        std::vector <std::string>   file_lines = plib::std_string::split(file_contents, '\n');
-
-        // TOML_Table                  current_table = root_table;
-        std::vector <std::string>   current_table_lines = {};
-
-
-        TOML_KV parsed_line;
-        TOML_Table* new_table;
-
-        TOML_String current_table_name;
-        TOML_String root_table_name = "ROOT_TABLE";
-
-        for(std::string line : file_lines){
-            // std::cout << line << std::endl;
-            
-            current_table_lines.push_back(line);
-
-            parsed_line = parse_line(line);
-
-            // Parse table if a new table indentifier is encountered
-            if(parsed_line.value.type == TOML_ValueType::TABLE){
-
-                // Do not include the next table label
-                current_table_lines.pop_back();
-
-                new_table = parse_table(current_table_lines);
-
-                if (root_table == nullptr) {
-                    root_table = new_table;
-                }
-                else {
-                    new_table->name = parsed_line.key;
-                }
-
-                tables.push_back(new_table);
-                
-                // Reset table lines
-                current_table_lines.clear();
-                current_table_lines.push_back(line);
-                // set name of new table that triggered parsing of preivous table
-                current_table_name = parsed_line.key;
-            }
-
+            parse_line_and_push_kv(line);
 
         }
-
-        // Make sure the last table is parsed
-        if (true) {
-            TOML_Table* new_table = parse_table(current_table_lines);
-
-            if (root_table == nullptr) {
-                root_table = new_table;
-                root_table->name = root_table_name;
-            }
-            else {
-                new_table->name = current_table_name;
-            }
-
-            tables.push_back(new_table);
-        }
-
-
-
-        std::cout << "TOML LOADING DONE" << std::endl;
-
     }
 
-    // Parse KV lines of table, including the table label itself ([table_name])
-    // Returns nullptr on empty table
-    TOML_Table* TOML::parse_table(std::vector<std::string> table_lines) {
-        
-        TOML_Table* return_table = new TOML_Table();
 
-        // Usually its the root table that contains no lines
-        if (table_lines.size() == 0){
-            return return_table;
-        }
 
-        for (std::string line : table_lines){
+    // Returns true if successfull parse. False on errors.
+    // Currently 3 types of lines : empty, table label, or KV containing a '='
+    bool     TOML_Table::parse_line_and_push_kv(std::string line) {
 
-            TOML_KV parsed_prop = parse_line(line);
-            
-            // Table name
-            if (parsed_prop.value.type == TOML_ValueType::TABLE){
-                return_table->name = parsed_prop.key;
-                continue;
-            }
-
-            // Empty line within table is skipped
-            if (parsed_prop.key == ""){
-                continue;
-            }
-            
-            return_table->kvs.push_back(parsed_prop);
-
-        }
-
-        return return_table;
-
-    }
-
-    // Currently 3 types of lines: empty, table label, or KV containing a '='
-    TOML_KV TOML::parse_line(std::string line) {
-
-        TOML_KV toml_kv;
-        toml_kv.value.type = TOML_ValueType::EMPTY;
-        toml_kv.key = "";
+        // TOML_Value new_value;
+        TOML_String key;
 
         if (line.empty())
-            return toml_kv;
+            return true;
 
-        // No '=' found --> table
+        // No '=' found --> Table Label
         bool is_table_label = line.find('=') == std::string::npos;
         if (is_table_label) {
 
-            toml_kv.value.type = TOML_ValueType::TABLE;
             pstring label_str = line;
             plib::std_string::trim(label_str);
-            toml_kv.key = label_str.substr(1, label_str.size() - 2);
 
-            bool table_format_ok = line[0] == '[' && line[line.size() - 1] == ']';
+            // bool table_format_ok = line[0] == '[' && line[line.size() - 1] == ']';
+            // if (!table_format_ok)
+            //     return false;
 
-            // if (!table_format_ok) {
-            //     prop.type = TOML_Type::EMPTY;
-            //     prop.name = "";
-            // }
+            TOML_String table_name_no_brackets = label_str.substr(1, label_str.size() - 2);
 
-            return toml_kv;
+            this->name = table_name_no_brackets;
+            return true;
+
         }
 
 
@@ -183,34 +129,67 @@ namespace plib {
         // First substring is the key
         std::string first_substr = line_kv_strings[0];
         plib::std_string::trim(first_substr);
-        toml_kv.key = first_substr;
+        key = first_substr;
 
 
         // VALUE
         std::string second_substr = line_kv_strings[1];
         plib::std_string::trim(second_substr);
 
-        TOML_Value parsed_value = parse_toml_value(second_substr);
-        toml_kv.value = parsed_value;
+        // TOML_Value parsed_value = parse_toml_value(second_substr);
+        TOML_Value toml_value = TOML_Value(second_substr);
 
 
-        return toml_kv;
+        kvs.push_back(TOML_KV(key, toml_value));
+
+        return true;
+    }
+
+
+    // END TOML TABLE
+
+
+    // START TOML VALUE
+
+
+    TOML_Value::TOML_Value(pstring value_string) {
+
+        // Type detection
+        char first_char = value_string[0];
+        switch (first_char)
+        {
+        case '"':
+        {
+            std::string str_without_quotes = value_string.substr(1, value_string.size() - 2);
+            string = str_without_quotes;
+            type = TOML_ValueType::STRING;
+        }
+        break;
+
+        case '[':
+        {
+            array = parse_array_single_line(value_string);
+            type = TOML_ValueType::ARRAY;
+        }
+        break;
+
+        default:
+            break;
+        }
 
     }
 
 
-
-    TOML_Array TOML::parse_single_line_array(pstring array_str) {
-
+    TOML_Array      TOML_Value::parse_array_single_line(pstring array_str) {
         TOML_Array toml_array = {};
 
         pstring array_str_no_brackets = array_str.substr(1, array_str.size() - 2);
 
         std::vector<pstring> array_str_entries = plib::std_string::split(array_str_no_brackets, ',');
 
-        for (pstring string_entry : array_str_entries){
+        for (pstring string_entry : array_str_entries) {
             plib::std_string::trim(string_entry);
-            TOML_Value parsed_value = parse_toml_value(string_entry);
+            TOML_Value parsed_value = TOML_Value(string_entry);
             toml_array.push_back(parsed_value);
         }
 
@@ -218,37 +197,40 @@ namespace plib {
     }
 
 
-    TOML_Value TOML::parse_toml_value(std::string value_str) {
+    void TOML_Value::set_value(TOML_String string) {
+        type = TOML_ValueType::STRING;
+        this->string = string;
+    }
+    void TOML_Value::set_value(TOML_Array array) {
+        type = TOML_ValueType::ARRAY;
+        this->array = array;
+    }
 
-        
-        TOML_Value toml_value;
 
-        // TYPE DETECTION
-        char first_char = value_str[0];
+    // END TOML VALUE
 
-        switch (first_char)
-        {
-        case '"':
-            {
-                std::string str_without_quotes = value_str.substr(1, value_str.size() - 2);
-                toml_value.set_value(str_without_quotes);
-            }
-            break;
+    
 
-        case '[':
-            {
-                TOML_Array toml_array = parse_single_line_array(value_str);
-                toml_value.set_value(toml_array);
-            }
-            break;
+    // START TOML KV
 
-        default:
-            break;
-        }
+    TOML_KV::TOML_KV(TOML_String _key, TOML_Value _value) {
+        key     = _key;
+        value   = _value;
+    }
 
-        return toml_value;
+    // Currently 3 types of lines: empty, table label, or KV containing a '='
+    bool TOML::is_table_label_line(std::string line) {
+
+        if (line.empty())
+            return false;
+
+        // No '=' found --> table
+        bool is_table_label = line.find('=') == std::string::npos;
+
+        return is_table_label;
 
     }
 
+    // END TOML KV
 
 }
--- src/lib/toml.hh
+++ src/lib/toml.hh
@@ -6,74 +6,108 @@
 
 namespace plib {
 
-    struct TOML_Value;
-    struct TOML_KV;
     struct TOML_Table;
-
+    struct TOML_KV;
+    class TOML_Value;
 
     typedef pstring                     TOML_String;
     typedef std::vector<TOML_Value>     TOML_Array;
 
-
+    /**
+     * Supported TOML Types:
+     *  - STRING
+     *  - ARRAY
+     */
     typedef enum TOML_ValueType {
-        EMPTY,
-        TABLE,
         STRING,
         ARRAY,
     } TOML_ValueType;
 
 
-    typedef struct TOML_Value {
-        TOML_ValueType type = TOML_ValueType::EMPTY;
+    /**
+     *   A parsed toml file
+     *   Notes:
+     *   Supported Types: STRING, ARRAY
+     *   Other: Only single line KV pairs
+     */
+    typedef class TOML {
+    public:
+        /**
+         *   A list of all tables in a loaded toml file.
+         *   All KV pairs before first table label is referred to as "ROOT_TABLE" and is always located at 'tables[0]'
+         */
+        std::vector<TOML_Table*>    tables;
 
-        TOML_String string;
-        TOML_Array  array;
+        /**
+         *  Load full TOML file from unverified string path
+         */
+        void            load(std::string file_spath);
 
-        void set_value(TOML_String string);
-        void set_value(TOML_Array array);
-        
+        /**
+         * Returns a table object with name matching argument.
+         * table_name = "ROOT_TABLE" || "" will return the root table.
+         * @return Match or nullptr
+         */
+        TOML_Table* find_table(TOML_String table_name);
 
-        TOML_Value() {};
-    } TOML_Value;
+        /**
+         *  Simple check during line parsing to see if a new table line is present.
+         */
+        bool            is_table_label_line(std::string line);
+
+        TOML() {};
+    } TOML;
 
 
-    typedef struct TOML_KV {
-        TOML_String key;
-        TOML_Value  value;
-    } TOML_KV;
-
-
+    /**
+     * Toml table identified by [table_name] in document and 'contains' a list of KV pairs.
+     */
     typedef struct TOML_Table {
         std::string name;
         std::vector<TOML_KV> kvs;
+
+        // Parse
+        bool     parse_line_and_push_kv(std::string line);
+
+        TOML_Table(std::vector<std::string> lines);
     } TOML_Table;
 
 
-    // A parsed toml file
-    // Format support notes:
-    // Types: STRING, ARRAY
-    // Other: Only single line KV pairs
-    typedef class TOML {
+    /**
+     *  A Generic toml data object that will hold any of the supported TOML types as specified in the 'TOML_ValueType' enum.
+     */
+    typedef class TOML_Value {
         public:
-            TOML_Table*                 root_table = nullptr;
-            std::vector<TOML_Table*>    tables;
+            TOML_ValueType type;
+
+            TOML_String string;
+            TOML_Array  array;
+
+        public:
+            /** Might remove in favor of private & immutable primitive values */
+            void set_value(TOML_String string);
+            /** Might remove in favor of private & immutable primitive values */
+            void set_value(TOML_Array array);
+
+            /** Only single line arrays currently supported. */
+            TOML_Array      parse_array_single_line(pstring array_str);
+
+            // Trimmed string to be parsed as a toml value
+            TOML_Value(pstring value_string);
+            TOML_Value() {};
+    } TOML_Value;
 
 
-            // Load full TOML file from unverified string path
-            void            load(std::string file_spath);
+    /**
+     * A pair of toml values. The key is always a string, and the value can hold any ofe the supported toml value types.
+     */
+    typedef struct TOML_KV {
+        TOML_String key;
+        TOML_Value  value;
 
-            // returns a table object with name matching argument.
-            TOML_Table*     find_table(TOML_String table_name);
-
-            TOML_Table*     parse_table(std::vector<std::string> lines);
-            TOML_KV         parse_line(std::string line);
-
-            TOML_Array      parse_single_line_array(pstring array_str);
-
-            TOML_Value      parse_toml_value(std::string value_str);
-
-            TOML() {};
-    } TOML;
+        TOML_KV(TOML_String _key, TOML_Value _value);
+        TOML_KV() {};
+    } TOML_KV;
 
 
 }
