62e4c94dba199cd219522133efabf7a32d564fd3
anders-erik <anderserikerik@gmail.com>
2026-08-25 12:24:10 +0200

    input: non-blocking read of evdev device;

--- src/input/main_input.cc
+++ src/input/main_input.cc
@@ -33,6 +33,74 @@ void user_info()
     system("id");
 }
 
+
+
+
+int read_and_print_keypresses()
+{
+    struct input_event
+    {
+        struct timeval time;
+        unsigned short type;
+        unsigned short code;
+        unsigned int value;
+    };
+
+    input_event ev;
+
+    const char* file_path = "/dev/input/event9";
+    int fd = open(file_path, O_RDONLY | O_NONBLOCK);
+    if(fd < 0)
+    {
+        printf("Error: failed to open file %c \n", file_path);
+        return 1;
+    }
+
+
+    int read_byte_count = read(fd, &ev, sizeof(input_event));
+
+    // while(read_byte_count > 0)
+    while(1)
+    {
+        if(ev.type == EV_KEY)
+        {
+
+            switch (ev.value)
+            {
+                case 0:
+                    printf("Key %d released \n", ev.code);
+                    break;
+                case 1:
+                    printf("Key %d pressed \n", ev.code);
+                    break;
+                case 2:
+                    printf("Key %d held \n", ev.code);
+                    break;
+                
+                default:
+                    break;
+            }
+        }
+        // printf("read ev.value = %d ", ev.value);
+        // print("\n");
+
+        // std::cout << std::flush;
+
+        sleep(1);
+        read_byte_count = read(fd, &ev, sizeof(input_event));
+        printf("read_byte_count = %d \n", read_byte_count);
+    }
+
+    int ret = close(fd);
+    if(ret < 0)
+    {
+        printf("Error: failed to close file %c \n", file_path);
+        return 1;
+    }
+
+    return 0;
+}
+
 int main()
 {
     print("~~ main input ~~ \n");
@@ -70,10 +138,17 @@ int main()
     // remove_current_user_to_input_group();
     // user_info();
 
+    // while(1)
+    {
+        // sleep(1);
+
+        read_and_print_keypresses();
+    }
+    // read_and_print_keypresses();
 
     // Trying to read /dev/input/event9
     // Source: https://stackoverflow.com/questions/15949163/read-from-dev-input
-    if(true)
+    if(false)
     {
         const char* file_path = "/dev/input/event9";
         int fd = open(file_path, O_RDONLY);
@@ -83,17 +158,17 @@ int main()
             return 1;
         }
         
-        struct input_event
-        {
-            struct timeval time;
-            unsigned short type;
-            unsigned short code;
-            unsigned int value;
-        };
+        // struct input_event
+        // {
+        //     struct timeval time;
+        //     unsigned short type;
+        //     unsigned short code;
+        //     unsigned int value;
+        // };
         // sizeof(input_event);
         input_event ev;
 
-
+        
 
         while(1)
         {
