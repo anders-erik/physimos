f903c0953fc50e13db03c6d1bc100c7c249a72c2
anders-erik <anderserikerik@gmail.com>
2026-09-02 10:37:16 +0200

    swrend: bitmap refactor;

--- src/swrend/main_swrend.cc
+++ src/swrend/main_swrend.cc
@@ -1,5 +1,6 @@
 
 #include <cstdint>
+#include <cstring>
 
 #include "lib/print.hh"
 #include "lib/arr.hh"
@@ -31,11 +32,28 @@ struct Pixel
     Str to_str()
     {
         return {
+            "(",
             Str::UI(r),
             ", ",
             Str::UI(g),
             ", ",
             Str::UI(b),
+            ")",
+        };
+    }
+    Str to_str_hex()
+    {
+        int  num = r << 24;
+        num += g << 16;
+        num += b << 8;
+
+        char hex[16];
+        memset(hex, 0, 16);
+
+        sprintf(hex, "%x", num);
+    
+        return {
+            Str(hex),
         };
     }
 };
@@ -45,7 +63,8 @@ typedef Arr<Pixel> Col;
 /** 24-bit pixel bitmap */
 class Bitmap
 {
-    Arr<Col> cols;
+    // Arr<Col> cols;
+    Vec<uint8_t> data;
 
     uint height = 0;
     uint width = 0;
@@ -57,32 +76,57 @@ public:
         height = _height;
         width = _width;
 
-        cols.clear();
+        data.set_size(height*width*3);
+        // data.set(0);
+        clear(0);
 
-        for(uint i = 0; i < width; i++)
-        {
-            cols.push_back({});
-            cols[i].set({}, height);
-        }
+        // for(uint i = 0; i < width; i++)
+        // {
+        //     cols.push_back({});
+        //     cols[i].set({}, height);
+        // }
     }
 
+
     // Returns a column of pixels at x=col_index
-    Col& operator[](uint _col_index)
+    // Col& operator[](uint _col_index)
+    // {
+    //     return cols[_col_index];
+    // }
+
+    uint stride()
     {
-        return cols[_col_index];
+        return 3 * width;
+    }
+
+    uint get_pixel_index(uint _x, uint _y)
+    {
+        return _y * stride() + _x;
+    }
+
+    Pixel& operator[](uint _x, uint _y)
+    {
+    
+        return (Pixel&) *(data.data_mut() + get_pixel_index(_x, _y));
     }
 
     void clear(Pixel _pixel)
     {
-        for(uint x = 0; x < width; x++)
+        for(uint p = 0; p < data.size(); p = p + 3)
         {
-            for(uint y = 0; y < height; y++)
-            {
-                (*this)[x][y] = _pixel;
-            }
+            data[p] = _pixel.r;
+            data[p+1] = _pixel.g;
+            data[p+2] = _pixel.b;
         }
     }
 
+    void clear(uint8_t _byte)
+    {
+        for(uint i = 0; i < width; i++)
+        {
+            data[i] = _byte;
+        }
+    }
 
     constexpr uint count_pixels()
     {
@@ -104,12 +148,17 @@ int main(int argc, const char** argv)
 
     Str pixel_str;
 
-    pixel_str = bmp[0][0].to_str();
+    pixel_str = bmp[0, 0].to_str();
     Print::ln(pixel_str);
 
-    bmp[0][0] = {255, 255, 255};
-    pixel_str = bmp[0][0].to_str();
+    bmp[0, 0] = {255, 255, 255};
+    pixel_str = bmp[0, 0].to_str();
     Print::ln(pixel_str);
+
+    bmp[0, 0] = {255, 255, 255};
+    Print::ln(bmp[0, 0].to_str_hex());
+    bmp[0, 0] = {0, 100, 200};
+    Print::ln(bmp[0, 0].to_str_hex());
     
 
     Print::ln("Bye, from main_swrend.cc. \n");
